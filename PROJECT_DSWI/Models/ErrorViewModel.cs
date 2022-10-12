561518fe86cd71e9666025b360dbec07a810fa7c
ncmendoza <nelzoncriollo@gmail.com>
2022-10-11 21:15:06 -0500

    configuracion del proyecto - nelson

---
+++ PROJECT_DSWI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROJECT_DSWI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

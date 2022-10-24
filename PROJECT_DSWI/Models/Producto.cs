52a2d4a70247703012e50ef06909f8647d73e0eb
ncmendoza <nelzoncriollo@gmail.com>
2022-10-23 22:55:12 -0500

    se agrego las clases producto y categoria

---
+++ PROJECT_DSWI/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace PROJECT_DSWI.Models
+{
+    public class Producto
+    {
+        public int idProducto { get; set; }
+        public string descripcion { get; set; }
+        public int idCategoria { get; set; }
+        public int stock { get; set; }
+    }
+}

c341b23f6b06c7a174e7e4f15ec51f7cdb295ebb
asoton <alfredosotonolazco@gmail.com>
2022-10-24 02:04:41 -0500

    pedido y pedido dao

--- PROJECT_DSWI/DAO/PedidoDAO.cs
+++ PROJECT_DSWI/DAO/PedidoDAO.cs
@@ -5,6 +5,34 @@ namespace PROJECT_DSWI.DAO
 {
     public class PedidoDAO
     {
+        public IEnumerable<Pedido> pedidos()
+        {
+            List<Pedido> temporal = new List<Pedido>();
+            ConexionDAO cn = new ConexionDAO();
+            using (cn.getcn)
+            {
+                cn.getcn.Open();
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader dr = cmd.ExecuteReader();
 
+                while (dr.Read())
+                {
+                    temporal.Add(new Pedido()
+                    {
+                        idPedido = dr.GetInt32(0),
+                        idCliente = dr.GetInt32(1),
+                        idTipoPedido = dr.GetInt32(2),
+                        fechaHoraPedido = dr.GetDateTime(3),
+                        totalPedido = dr.GetDecimal(4),
+                        idMetodoPago = dr.GetInt32(5),
+                        cod_Ubigeo = dr.GetString(6),
+                        direccionPedido = dr.GetString(7),
+                        estado = dr.GetInt32(8)
+                    });
+                }
+                cn.getcn.Close();
+            }
+            return temporal;
+        }
     }
 }
--- PROJECT_DSWI/Models/Pedido.cs
+++ PROJECT_DSWI/Models/Pedido.cs
@@ -1,16 +1,30 @@
-﻿namespace PROJECT_DSWI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace PROJECT_DSWI.Models
 {
     public class Pedido
     {
         public int idPedido { get; set; }
         public int idCliente { get; set; }
         public int idTipoPedido { get; set; }
-        public DateTime fechaHoraPedido { get; set; }
-        public decimal totalPedido { get; set; }
-        public int idMetodoPago { get; set; }
-        public string cod_Ubigeo { get; set; }
-        public string direccionPedido { get; set; }
-        public int estado { get; set; }
+        [Display(Name = "Fecha"),Required]public DateTime fechaHoraPedido { get; set; }
+        [Display(Name = "Total"), Required] public decimal totalPedido { get; set; }
+        [Display(Name = "Método"), Required] public int idMetodoPago { get; set; }
+        [Display(Name = "Ubigeo"), Required,StringLength(10)] public string cod_Ubigeo { get; set; }
+        [Display(Name = "Dirección"), Required, StringLength(30)] public string direccionPedido { get; set; }
+        [Display(Name = "Estado"), Required, StringLength(10)] public int estado { get; set; }
+        public Pedido()
+        {
+            idPedido = 0;
+            idCliente = 0;
+            idTipoPedido = 0;
+            fechaHoraPedido = DateTime.Now;
+            totalPedido = 0;
+            idMetodoPago = 0;
+            cod_Ubigeo = "";
+            direccionPedido = "";
+            estado = 0;
+        }
     }
-}
 }

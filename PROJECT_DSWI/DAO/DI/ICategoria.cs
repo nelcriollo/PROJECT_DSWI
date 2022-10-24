06c6871b20d1265dfa8f70d2a57ff3d60b03be93
ncmendoza <nelzoncriollo@gmail.com>
2022-10-24 00:55:55 -0500

    se agrego los métodos en las interfaces Icategoria e IProducto

--- PROJECT_DSWI/DAO/DI/ICategoria.cs
+++ PROJECT_DSWI/DAO/DI/ICategoria.cs
@@ -1,6 +1,12 @@
-﻿namespace PROJECT_DSWI.DAO.DI
+﻿using PROJECT_DSWI.Models;
+
+namespace PROJECT_DSWI.DAO.DI
 {
     public interface ICategoria
     {
+        IEnumerable<CategoriaDAO> GetCategorias();
+        string insertCategoria();
+        string ActualizarCategoria();
+
     }
 }
--- PROJECT_DSWI/DAO/DI/IProducto.cs
+++ PROJECT_DSWI/DAO/DI/IProducto.cs
@@ -1,6 +1,11 @@
-﻿namespace PROJECT_DSWI.DAO.DI
+﻿using PROJECT_DSWI.Models;
+
+namespace PROJECT_DSWI.DAO.DI
 {
     public interface IProducto
     {
+        IEnumerable<ProductoDAO> GetAll();
+        string RegistrarProducto(Producto reg);
+        string ActualizarProducto(Producto reg);
     }
 }

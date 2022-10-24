ab040fe0d7cf6f2adfe2fbbe313c5b28ea9f61d6
ncmendoza <nelzoncriollo@gmail.com>
2022-10-23 23:14:32 -0500

    se agrego la clase conexion

---
+++ PROJECT_DSWI/DAO/Conexioncs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace PROJECT_DSWI.DAO
+{
+    public class Conexioncs
+    {
+        SqlConnection cn = new SqlConnection(@"server=NELSON\MSSQLSERVER2;DataBase= BD_SaborCriollo_v1.0.2;" +
+      "Trusted_Connection = False; uid = sa; pwd = 123456; MultipleActiveResultSets = True; TrustServerCertificate = False; Encrypt = False;");
+
+        // lectura de la conexion
+        public SqlConnection getcn
+        {
+            get { return cn; }
+        }
+    }
+}

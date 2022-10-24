d0550175c0791fabe85a65173fdc86108999b8a5
asoton <alfredosotonolazco@gmail.com>
2022-10-23 23:49:45 -0500

    clases pedido y cliente agregadas

---
+++ PROJECT_DSWI/Models/Pedido.cs
@@ -0,0 +1,16 @@
+﻿namespace PROJECT_DSWI.Models
+{
+    public class Pedido
+    {
+        public int idPedido { get; set; }
+        public int idCliente { get; set; }
+        public int idTipoPedido { get; set; }
+        public DateTime fechaHoraPedido { get; set; }
+        public decimal totalPedido { get; set; }
+        public int idMetodoPago { get; set; }
+        public string cod_Ubigeo { get; set; }
+        public string direccionPedido { get; set; }
+        public int estado { get; set; }
+    }
+}
+}

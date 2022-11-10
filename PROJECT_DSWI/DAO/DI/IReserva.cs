9be18f9416ae34f471fef02d5713d1cd04855e81
ebrp-c <ebrp.1290@gmail.com>
2022-11-10 00:51:54 -0500

    actualizacion Reserva DAO, Interface Reserva parcialmente

---
+++ PROJECT_DSWI/DAO/DI/IReserva.cs
@@ -0,0 +1,10 @@
+﻿using PROJECT_DSWI.Models;
+
+namespace PROJECT_DSWI.DAO.DI
+{
+    public interface IReserva
+    {
+        IEnumerable<Reserva> listarReservaCliente(string doc);
+        string RegistrarReserva(Reserva reg);        
+    }
+}
---
+++ PROJECT_DSWI/DAO/ReservaDAO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using PROJECT_DSWI.Models;
+using PROJECT_DSWI.DAO.DI;
+
+namespace PROJECT_DSWI.DAO
+{
+    public class ReservaDAO : IReserva
+    {
+        public IEnumerable<Reserva> listarReservaCliente(string doc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RegistrarReserva(Reserva reg)
+        {
+            string mensaje = "";
+            ConexionDAO cn = new ConexionDAO();
+            using (cn.getcn) 
+            {
+                cn.getcn.Open();
+                try
+                {
+                    SqlCommand cmc = new SqlCommand();
+                }
+                catch (SqlException ex)
+                {
+                    mensaje = ex.Message;
+                }
+                finally { cn.getcn.Close(); }
+            }
+            return mensaje;
+        }
+    }
+}

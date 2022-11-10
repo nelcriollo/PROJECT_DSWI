5523e3fa8806e1078b6c8f12a3cc27f1060087a7
Franco <Franco1496>
2022-11-10 01:54:31 -0500

    Implementando parcialmente DAO Interface Controller DetallePedido

---
+++ PROJECT_DSWI/Controllers/DetallePedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROJECT_DSWI.Controllers
+{
+    public class DetallePedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

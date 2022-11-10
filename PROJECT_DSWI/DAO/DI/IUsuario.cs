c8cb8aee97caa1f423dea23086e155c96d68da8d
Franco <Franco1496>
2022-11-10 01:18:02 -0500

    implementacion de Controler e Interface Usuario parcial

--- PROJECT_DSWI/DAO/DI/IUsuario.cs
+++ PROJECT_DSWI/DAO/DI/IUsuario.cs
@@ -4,5 +4,9 @@ namespace PROJECT_DSWI.DAO.DI
 {
     public interface IUsuario
     {
+
+        IEnumerable<Usuario> listarUsuarios();
+        string RegistrarUsuario(Usuario reg);
+        string ActualizarUsuario(Usuario reg);
     }
 }

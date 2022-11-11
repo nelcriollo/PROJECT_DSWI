6cb4a34ce2d1276b9a71dd920318276495af59a1
n3o4lfr3d0 <alfredosotonolazco@gmail.com>
2022-11-11 02:38:21 -0500

    se avanzo pedido dao falta metodo eliminar

--- PROJECT_DSWI/DAO/ConexionDAO.cs
+++ PROJECT_DSWI/DAO/ConexionDAO.cs
@@ -3,16 +3,17 @@ namespace PROJECT_DSWI.DAO
 {
     public class ConexionDAO
     {
-<<<<<<< HEAD
-
 
         SqlConnection cn = new SqlConnection(@"server = LAPTOP-BB5T8A26\MSSQLSERVER01;database = SABORCRIOLLO;Trusted_Connection = True;" +
         "MultipleActiveResultSets = True;TrustServerCertificate = False;Encrypt = False ");
 
-=======
-        SqlConnection cn = new SqlConnection(@"server = EROJASP;database = SABORCRIOLLO;Trusted_Connection = True;" +
+        /*SqlConnection cn = new SqlConnection(@"server = EROJASP;database = SABORCRIOLLO;Trusted_Connection = True;" +
          "MultipleActiveResultSets = True;TrustServerCertificate = False;Encrypt = False");
->>>>>>> 06b005b749a44a2c8b8fe75645b858ad8cb09879
+        >>>>>>> 06b005b749a44a2c8b8fe75645b858ad8cb09879
+
+        SqlConnection cn = new SqlConnection(@"server = ASOTON;database = SABORCRIOLLO;Trusted_Connection = True;" +
+         "MultipleActiveResultSets = True;TrustServerCertificate = False;Encrypt = False");
+        >>>>>>> 06b005b749a44a2c8b8fe75645b858ad8cb09879*/
 
         // lectura de la conexion
         public SqlConnection getcn
